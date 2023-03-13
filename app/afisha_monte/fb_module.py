beea640b96fb404b5e68cf1699a5862505ff9786
hhux <hiphopuserx@gmail.com>
2023-03-13 18:58:19 +0100

    facebook post logic

--- app/afisha_monte/fb_module.py
+++ app/afisha_monte/fb_module.py
@@ -1,5 +1,5 @@
 from facebook_scraper import get_posts
-
+from facebook_scraper_in import *
 # FB_LOGIN = os.environ['FB_LOGIN']
 # FB_PASSWORD = os.environ['FB_PASSWORD']
 FB_LOGIN = '+382067365851'
@@ -7,10 +7,11 @@ FB_PASSWORD = 'Shtanga228'
 
 
 def get_last_fb_post(url):
+    facebook.login_password(phone_number='+382067365851', Password='Shtanga228')
     posts = get_posts(
         post_urls=[url],
-        # credentials=(FB_LOGIN, FB_PASSWORD),
+        credentials=(FB_LOGIN, FB_PASSWORD),
         options={"comments": True},
-        cookies='../cookie.txt'
+        # cookies='cookie.txt'
     )
     return next(posts, {})
