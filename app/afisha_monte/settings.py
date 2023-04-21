dc5d44910a3fa4022a852ec63e73931b24b5348a
hhux <hiphopuserx@gmail.com>
2023-04-21 17:01:41 +0200

    pagination 20 to 100

--- app/afisha_monte/settings.py
+++ app/afisha_monte/settings.py
@@ -90,7 +90,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication']
 }
 
